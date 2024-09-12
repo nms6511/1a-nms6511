938990f419cc0f4975de937cdc2b96c5eb109692
Nihal Sahul Hameed <nms6511@psu.edu>
2024-09-12 16:30:23 -0400

    changed git username

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "nms6511";
+    char STUDENT_ID[] = "95965030iiii0";
 
     return 0 ;
 }
